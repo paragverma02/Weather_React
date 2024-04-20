c5401d9f986baeb7dc19f8b08530672b17a7b5ce
paragverma02 <parag_ihm@yahoo.com>
2024-04-20 14:15:25 +0530

    weather project complete

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import WeatherApps from "./WeatherApps";
+import './App.css';
+
+function App() {
+  return (<>
+
+    <WeatherApps/>
+  </>  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "image":"url('/src/media/bg.jpg')",
+        "left":"url('/src/media/right.jpg')"}
+    },
+  },
+  plugins: [],
+}
